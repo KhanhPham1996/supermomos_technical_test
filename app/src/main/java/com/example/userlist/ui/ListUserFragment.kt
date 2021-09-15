10f247da1d76bd99489a59bc5843d3aae16dcc6f
Phạm Hoàng Việt Khánh (BM Herry) <khanhphv@brickmate.kr>
2021-09-15 15:01:00 +0700

    - Change File name

--- app/src/main/java/com/example/userlist/ui/ListUserFragment.kt
+++ app/src/main/java/com/example/userlist/ui/ListUserFragment.kt
@@ -1,13 +1,12 @@
 package com.example.userlist.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -70,7 +69,7 @@ class ListUserFragment : Fragment() {
 
     private fun onSwipeRefresh() {
        userViewModel.getUserList(requireContext(),false)
-        ssss
+        ádasdasdsad
     }
 
     private fun fetchDataFromServer() {
